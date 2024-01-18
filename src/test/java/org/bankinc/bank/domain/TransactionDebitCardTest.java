bdd4a7a1f7552140aa9c48e6e3e02120943cf9a3
Luis Fernando Pinzon Marroquin <59232095+Ludocius@users.noreply.github.com>
2024-01-17 22:56:51 -0500

    :recycle: unused imports deleted and Long literal left as upperCase

--- src/test/java/org/bankinc/bank/domain/TransactionDebitCardTest.java
+++ src/test/java/org/bankinc/bank/domain/TransactionDebitCardTest.java
@@ -3,18 +3,16 @@ package org.bankinc.bank.domain;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionDebitCardTest {
 
     @Test
     void create_debit_Card_Object() {
         TransactionDebitCard transactionDebitCard = TransactionDebitCard.builder()
-                .transactionId(3000l)
+                .transactionId(3000L)
                 .price(BigDecimal.valueOf(2002))
                 .transactionDate(LocalDate.now())
                 .isDebitCardActie(Boolean.TRUE)
